f1e2eb25ed00dcb387177d5b8727fdd73e40b4f5
UranoYushiro0412 <165639099+UranoYushiro0412@users.noreply.github.com>
2025-12-08 13:47:19 +0900

    Add w08_task2_main.js

--- W08/w08_task2_main.js
+++ W08/w08_task2_main.js
@@ -1,35 +1,29 @@
 d3.csv("https://UranoYushiro0412.github.io/InfoVis2025/W08/w08_task2.csv")
     .then( data => {
-        data.forEach( d => { d.population = +d.population; });
+        data.forEach( d => { d.x = +d.x; d.y = +d.y; });
 
         var config = {
             parent: '#drawing_region',
             width: 512,
             height: 512,
-            margin: {top:80, right:20, bottom:80, left:100},
-            title: "Population of Kinki Region Prefectures",
-            xlabel: "Prefecture",
-            ylabel: "Population"
+            margin: {top:20, right:20, bottom:40, left:40},
         };
 
-        const bar_chart = new BarChart( config, data );
-        bar_chart.update();
+        const line_chart = new LineChart( config, data );
+        line_chart.update();
     })
     .catch( error => {
         console.log( error );
     });
 
-class BarChart {
+class LineChart {
 
     constructor( config, data ) {
         this.config = {
             parent: config.parent,
             width: config.width || 512,
             height: config.height || 512,
-            margin: config.margin || {top:80, right:20, bottom:80, left:100},
-            title: config.title,
-            xlabel: config.xlabel,
-            ylabel: config.ylabel
+            margin: config.margin || {top:20, right:20, bottom:40, left:40},
         }
         this.data = data;
         this.init();
@@ -48,15 +42,14 @@ class BarChart {
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
-        self.xscale = d3.scaleBand()
-            .range( [0, self.inner_width] )
-            .paddingInner(0.1)
-            .paddingOuter(0.2);
+        self.xscale = d3.scaleLinear()
+            .range( [0, self.inner_width] );
     
         self.yscale = d3.scaleLinear()
             .range( [self.inner_height, 0] );
 
         self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(5)
             .tickSizeOuter(0);
 
         self.xaxis_group = self.chart.append('g')
@@ -66,39 +59,26 @@ class BarChart {
             .ticks(5)
             .tickSizeOuter(0);
 
-        self.yaxis_group = self.chart.append('g')
+        self.yaxis_group = self.chart.append('g');
 
-        self.svg.append("text")
-        .attr("x", self.config.width / 2)
-        .attr("y", self.config.margin.top / 2)
-        .attr("text-anchor", "middle")
-        .style("font-size", "24px")
-        .style("font-weight", "bold")
-        .text(self.config.title);
+        self.line = d3.line()
+            .x( d => self.xscale(d.x) )
+            .y( d => self.yscale(d.y) );
 
-        self.svg.append("text")
-            .attr("x", self.config.margin.left + self.inner_width / 2)
-            .attr("y", self.config.height - self.config.margin.bottom / 3)
-            .attr("text-anchor", "middle")
-            .style("font-size", "20px")
-            .text(self.config.xlabel);
-
-        self.svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", self.config.margin.left / 4)
-            .attr("x", -self.config.margin.top - (self.inner_height / 2))
-            .attr("text-anchor", "middle")
-            .style("font-size", "20px")
-            .text(self.config.ylabel);
+        self.area = d3.area()
+            .x( d => self.xscale(d.x) )
+            .y1( d => self.yscale(d.y) )
+            .y0( self.yscale(0) );
     }
 
     update() {
         let self = this;
 
-        self.xscale.domain(self.data.map(d => d.label) );
+        const xmax = d3.max( self.data, d => d.x );
+        self.xscale.domain( [0, xmax * 1.05] );
 
-        const ymax = d3.max( self.data, d => d.population );
-        self.yscale.domain( [0, ymax * 1.15] );
+        const ymax = d3.max( self.data, d => d.y );
+        self.yscale.domain( [0, ymax * 1.05] );
 
         self.render();
     }
@@ -106,15 +86,29 @@ class BarChart {
     render() {
         let self = this;
 
-        self.chart.selectAll("rect")
+        self.chart.selectAll(".area")
+            .data([self.data])
+            .join('path')
+            .attr('class', 'area')
+            .attr('d', self.area)
+            .attr('fill', 'lightblue')
+            .attr('opacity', 0.5);
+
+        self.chart.selectAll(".line")
+            .data([self.data])
+            .join('path')
+            .attr('d', self.line)
+            .attr('stroke', 'black')
+            .attr('fill', 'none')
+            .attr('stroke-width', 2);
+
+        self.chart.selectAll("circle")
             .data(self.data)
-            .enter()
-            .append("rect")
-            .attr("x", d => self.xscale(d.label))
-            .attr("y", d => self.yscale(d.population)) 
-            .attr("height", d => self.inner_height - self.yscale(d.population))
-            .attr("width", self.xscale.bandwidth())
-            .style("fill", function(d){ return d.color; });
+            .join('circle')
+            .attr("cx", d => self.xscale(d.x))
+            .attr("cy", d => self.yscale(d.y))
+            .attr("r", 5)
+            .attr("fill", "black");
 
         self.xaxis_group
             .call( self.xaxis );
@@ -124,5 +118,8 @@ class BarChart {
 
         self.yaxis_group
             .call( self.yaxis );
+        
+        self.yaxis_group.selectAll('text')
+            .style('font-size', '12px');
     }
 }
