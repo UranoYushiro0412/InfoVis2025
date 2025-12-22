87d843e1bb1dc10240116ede107dc56dd9423283
UranoYushiro0412 <165639099+UranoYushiro0412@users.noreply.github.com>
2025-12-22 14:57:33 +0900

    Add w10_task2_main.js

---
+++ W10/w10_task2_main.js
@@ -0,0 +1,149 @@
+d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2021/W04/data.csv")
+    .then( data => {
+        data.forEach( d => { d.x = +d.x; d.y = +d.y; d.r = +d.r; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 512,
+            height: 512,
+            margin: {top:80, right:20, bottom:80, left:80},
+            title: "Sample Data",
+            xlabel: "X-label",
+            ylabel: "Y-label"
+        };
+
+        const scatter_plot = new ScatterPlot( config, data );
+        scatter_plot.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+class ScatterPlot {
+
+    constructor( config, data ) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 512,
+            height: config.height || 512,
+            margin: config.margin || {top:80, right:20, bottom:80, left:80},
+            title: config.title,
+            xlabel: config.xlabel,
+            ylabel: config.ylabel
+        }
+        this.data = data;
+        this.init();
+    }
+
+    init() {
+        let self = this;
+
+        self.svg = d3.select( self.config.parent )
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+        self.xscale = d3.scaleLinear()
+            .range( [0, self.inner_width] );
+
+        self.yscale = d3.scaleLinear()
+            .range( [self.inner_height, 0] );
+
+        self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(10);
+
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
+
+        self.yaxis = d3.axisLeft( self.yscale )
+            .ticks(10);
+
+        self.yaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, 0)`);
+
+        self.svg.append("text")
+        .attr("x", self.config.width / 2)
+        .attr("y", self.config.margin.top / 2)
+        .attr("text-anchor", "middle")
+        .style("font-size", "32px")
+        .style("font-weight", "bold")
+        .text(self.config.title);
+
+        self.svg.append("text")
+            .attr("x", self.config.margin.left + self.inner_width / 2)
+            .attr("y", self.config.height - self.config.margin.bottom / 3)
+            .attr("text-anchor", "middle")
+            .style("font-size", "24px")
+            .text(self.config.xlabel);
+
+        self.svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", self.config.margin.left / 2)
+            .attr("x", -self.config.margin.top - (self.inner_height / 2))
+            .attr("text-anchor", "middle")
+            .style("font-size", "24px")
+            .text(self.config.ylabel);
+    }
+
+    update() {
+        let self = this;
+
+        const xmin = d3.min( self.data, d => d.x );
+        const xmax = d3.max( self.data, d => d.x );
+        self.xscale.domain( [0, xmax * 1.15] );
+
+        const ymin = d3.min( self.data, d => d.y );
+        const ymax = d3.max( self.data, d => d.y );
+        self.yscale.domain( [0, ymax * 1.15] );
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+        const circles = self.chart.selectAll("circle")
+            .data(self.data)
+            .join("circle")
+            .attr("cx", d => self.xscale( d.x ) )
+            .attr("cy", d => self.yscale( d.y ) )
+            .attr("r", d => d.r )
+            .attr("fill", "black");
+        
+        circles
+            .on('mouseover', (e, d) => {
+                d3.select('#tooltip')
+                    .style('opacity', 1)
+                    .html(`<div class="tooltip-label">Position</div>(${d.x}, ${d.y})`);
+            })
+            .on('mousemove', (e, d) => {
+                const padding = 10;
+                d3.select('#tooltip')
+                    .style('left', (e.pageX + padding) + 'px')
+                    .style('top', (e.pageY + padding) + 'px');
+                
+                d3.select(e.target)
+                    .attr('fill', 'orange')
+                    // .attr('r', d.r * 1.5);
+            })
+            .on('mouseleave', (e, d) => {
+                d3.select('#tooltip')
+                    .style('opacity', 0);
+                
+                d3.select(e.target)
+                    .attr('fill', 'black')
+                    // .attr('r', d.r);
+            });
+
+        self.xaxis_group
+            .call( self.xaxis );
+
+        self.yaxis_group
+            .call( self.yaxis );
+    }
+}
